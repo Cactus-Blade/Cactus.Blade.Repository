003e3e98bc1a9ba1adc0d2fe15319c85ebdb06eb
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-03-04 12:48:43 +0330

    Paginate - PaginateExtensions

---
+++ src/Cactus.Blade.Repository/Paging/PaginateExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cactus.Blade.Repository.Paging
+{
+    public static class PaginateExtensions
+    {
+        public static IPaginate<T> ToPaginate<T>(this IEnumerable<T> sources, int index, int size, int from = 0) =>
+            new Paginate<T>(sources, index, size, from);
+
+        public static IPaginate<TResult> ToPaginate<TSource, TResult>(this IEnumerable<TSource> sources,
+            Func<IEnumerable<TSource>, IEnumerable<TResult>> converter, int index, int size, int from = 0) =>
+            new Paginate<TSource, TResult>(sources, converter, index, size, @from);
+    }
+}
