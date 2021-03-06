a475da10a721d61c7555c949c0d5390bd4b6315c
Mohammad Sadeq Sirjani <45625225+msadeqsirjani@users.noreply.github.com>
2021-03-06 10:47:04 +0330

    Add Cactus.Blade.Repository Database Test

---
+++ test/Cactus.Blade.Repository.Database/TestCategory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cactus.Blade.Repository.Database
+{
+    public class TestCategory
+    {
+        public TestCategory()
+        {
+            Products = new HashSet<TestProduct>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required] public string Name { get; set; }
+
+        public ICollection<TestProduct> Products { get; set; }
+    }
+}
---
+++ test/Cactus.Blade.Repository.Database/TestProduct.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cactus.Blade.Repository.Database
+{
+    public class TestProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public TestCategory Category { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public int Stock { get; set; }
+
+        public bool? InStock { get; set; }
+    }
+}
